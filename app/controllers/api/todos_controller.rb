1e28539ec88b4c95d5ed25f417cdac1f8409609c
Asif Meer <asif@supertec.com>
2019-01-26 16:23:06 +0500

    todo api show action done

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -7,6 +7,17 @@ module Api
 			render json: {:message => "todos list", :status => "SUCCESS", :data => todos}, status: :ok			
 		end
 
+		def show
+
+			begin
+			  todo = Todo.find params[:id]
+			rescue ActiveRecord::RecordNotFound => e
+			  todo = nil
+			end
+			# todo = Todo.find(params[:id])
+			render json: {:message => "todo data", :status => "SUCCESS", :data => todo}, status: :ok			
+		end
+
 	end
 
 
