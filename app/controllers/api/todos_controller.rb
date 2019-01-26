39b615aec82538d6bc50e10b91524d54287937be
Asif Meer <asif@supertec.com>
2019-01-26 16:43:00 +0500

    create functionality done

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,6 +1,7 @@
 module Api
 
 	class TodosController < ApplicationController
+		skip_before_action :verify_authenticity_token
 
 		def index
 			todos = Todo.all
@@ -14,8 +15,26 @@ module Api
 			rescue ActiveRecord::RecordNotFound => e
 			  todo = nil
 			end
-			# todo = Todo.find(params[:id])
+
 			render json: {:message => "todo data", :status => "SUCCESS", :data => todo}, status: :ok			
+		end
+
+
+		def create
+			todo = Todo.new(todo_params)
+
+			if todo.save
+				render json: {:message => "todo saved sucessfully", :status => "SUCCESS", :data => todo}, status: :ok
+			else
+				render json: {:message => "unable to save", :status => "ERROR", :data => todo}, status: :ok
+			end
+			
+		end
+
+		private 
+
+		def todo_params
+			params.permit(:name,:description)
 		end
 
 	end
