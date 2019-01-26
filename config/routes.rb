811e6b4f8ed7a6b9e2fd9d0a211b6e9c060859bd
Asif Meer <asif@supertec.com>
2019-01-26 16:02:44 +0500

    TODO api routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace 'api' do
+
+    resources :todos
+
+  end
+
 end
