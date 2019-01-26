8a0664609de546907ca2ff7c393ff141d112b165
Asif Meer <asif@supertec.com>
2019-01-26 16:10:58 +0500

    db saample data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Load sample data to db
+Todo.create(:name => 'Sample data 1', :description => 'Sample data 1 description')
+Todo.create(:name => 'Sample data 2', :description => 'Sample data 2 description')
+Todo.create(:name => 'Sample data 3', :description => 'Sample data 3 description')
+Todo.create(:name => 'Sample data 4', :description => 'Sample data 4 description')
+Todo.create(:name => 'Sample data 5', :description => 'Sample data 5 description')
+Todo.create(:name => 'Sample data 6', :description => 'Sample data 6 description')
+Todo.create(:name => 'Sample data 7', :description => 'Sample data 7 description')
+Todo.create(:name => 'Sample data 8', :description => 'Sample data 8 description')
